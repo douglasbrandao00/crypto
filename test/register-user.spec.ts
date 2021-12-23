18e285134d563a728aa36ab19c9995950de60078
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2021-12-23 13:10:39 -0300

    fix: pass a arrow function to expect throw

--- test/register-user.spec.ts
+++ test/register-user.spec.ts
@@ -29,39 +29,43 @@ function userCandidateMock(): UserCandidate{
 describe("RegisterUser", () => {
   test('Shoud return undefined if user candidate is valid', () => {
     const userCandidate = userCandidateMock()
+    
     const registerUser = new RegisterUser(userCandidate)
 
     expect(registerUser.handle()).toEqual(undefined)
-
   })
 
   test("Shoud throw if user name is a empty string", () => {
     const userCandidate = userCandidateMock()
     userCandidate.name = ""
+    
     const registerUser = new RegisterUser(userCandidate)
 
-    expect(registerUser.handle).toThrow()
+    expect(() => registerUser.handle()).toThrow()
   })
 
   test("Shoud throw if user email is a empty string", () => {
     const userCandidate = userCandidateMock()
     userCandidate.email = ""
+    
     const registerUser = new RegisterUser(userCandidate)
 
-    expect(registerUser.handle).toThrow()
+    expect(() => registerUser.handle()).toThrow()
   })
   test("Shoud throw if user password is a empty string", () => {
     const userCandidate = userCandidateMock()
     userCandidate.password = ""
+    
     const registerUser = new RegisterUser(userCandidate)
 
-    expect(registerUser.handle).toThrow()
+    expect(() => registerUser.handle()).toThrow()
   })
   test("Shoud throw if user confirmPassword is a empty string", () => {
     const userCandidate = userCandidateMock()
     userCandidate.confirmPassword = ""
+
     const registerUser = new RegisterUser(userCandidate)
 
-    expect(registerUser.handle).toThrow()
+    expect(() => registerUser.handle()).toThrow()
   })
 })
