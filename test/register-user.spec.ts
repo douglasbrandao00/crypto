34dc730cf3c90c42fe29c4ef525bc8d6436d2c9b
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2021-12-23 12:19:04 -0300

    test: ensure RegisterUser throws if user name is empty

--- test/register-user.spec.ts
+++ test/register-user.spec.ts
@@ -1,9 +1,34 @@
-import * as japa from 'japa'
+type UserCandidate = {
+      name: string
+      email: string
+      password: string
+      confirmPassword:string
+}
 
-const { test } = japa
+class RegisterUser {
+  constructor(private readonly candidate: UserCandidate) {}
 
-test.group('Group', () => {
-  test('Shoud return true ', (assert) => {
-    assert.equal(true, true) 
- 	})
- })
+  handle() {
+    throw new Error()
+  }
+}
+
+function userCandidateMock(): UserCandidate{
+  return {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+    confirmPassword: 'any_password',
+  }
+
+}
+
+describe("RegisterUser", () => {
+  test("Shoud throw if user name is a empty string", () => {
+    const userCandidate = userCandidateMock()
+    userCandidate.name = ""
+    const registerUser = new RegisterUser(userCandidate)
+
+    expect(registerUser.handle).toThrow()
+  })
+})
