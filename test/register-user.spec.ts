c0bd0d2282ee0c055294f34e8544251031c3104d
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2021-12-23 22:47:24 -0300

    refactor: move use case to its on file

--- test/register-user.spec.ts
+++ test/register-user.spec.ts
@@ -1,30 +1,4 @@
-type UserCandidate = {
-      name: string
-      email: string
-      password: string
-      confirmPassword:string
-}
-
-class RegisterUser {
-  constructor(private readonly candidate: UserCandidate) {}
-
-  handle() {
-    const candidate = this.candidate
-    if(!candidate.name || !candidate.email || !candidate.password || !candidate.confirmPassword) {
-      throw new Error()
-    }
-    const accetableEspecialCharacters = /[!@#$%^&*()_+\-=\[\]{};:\\|,.<>\/?]+/
-    const unaccetableEspecialCharacters = /^['"`]*$/
-    const passwordNumberAmount = candidate.password.replace(/\D/g, '').length
-
-    if(candidate.name.length < 4) throw new Error()
-    if(candidate.password !== candidate.confirmPassword) throw new Error()
-    if(candidate.password.length < 8) throw new Error()
-    if(passwordNumberAmount < 1) throw new Error()
-    if(unaccetableEspecialCharacters.test(candidate.password)) throw new Error()
-    if(!accetableEspecialCharacters.test(candidate.password)) throw new Error()
-  }
-}
+import {UserCandidate, RegisterUser} from '../src/domain/usecases/register-user'
 
 function userCandidateMock(): UserCandidate{
   return {
